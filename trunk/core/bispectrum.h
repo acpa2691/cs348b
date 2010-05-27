50cd8c087c0b7f3f547f9a0b663cd65891e48b36
Rahul.q.Agarwal <Rahul.q.Agarwal@a32b58a0-90e8-a01c-5bb8-2595f58187fd>
2010-05-27 03:52:09 +0000

    added bispeectrum class

---
+++ trunk/core/bispectrum.h
@@ -0,0 +1,24 @@
+#pragma once 
+#include "color.h"
+#include <iostream> 
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+class COREDLL Bispectrum {
+ public:
+  // Spectrum Public Methods
+
+  Bispectrum(string &filename) {
+
+    ifstream infile;
+    infile.open
+    
+  }
+
+ private:
+  vector<float> wavelengths;
+  vector<vector<float> > radiation_matrix;
+	//cs needs to have sive SPECTRUM_SAMPLES
+
+  };
